f3c1c01eea5195d800752173f29843827a7d3725
김영현 <kimyounghyeon@gim-yeonghyeon-ui-MacBookPro.local>
2025-02-02 21:08:57 +0900

    Initial commit for Azure web app

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ st.write("이건 Streamlit으로 만든 간단한 웹 앱입니다.")
 
 name = st.text_input("이름을 입력하세요:")
 if st.button("인사하기"):
-    st.write(f"안녕하세요, {name}님! Streamlit에 오신 것을 환영합니다.")
+    st.write(f"안녕하세요, {name}님! Streamlit에 오신 것을 환영합니다. 반가워요~")
