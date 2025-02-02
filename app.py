4c8ec0ff546fa6b5d8e600ff402439717876372a
김영현 <kimyounghyeon@gim-yeonghyeon-ui-MacBookPro.local>
2025-02-02 20:02:37 +0900

    streamlit test

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Hello, Streamlit!")
+st.write("이건 Streamlit으로 만든 간단한 웹 앱입니다.")
+
+name = st.text_input("이름을 입력하세요:")
+if st.button("인사하기"):
+    st.write(f"안녕하세요, {name}님! Streamlit에 오신 것을 환영합니다.")
