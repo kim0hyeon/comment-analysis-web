c895b2e79fca27ed9deb5a5eddcb5ea5fdcdc86b
kim0hyeon <dbfwjs991105@gmail.com>
2025-02-03 12:29:56 +0900

    englist analysis test

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 import streamlit as st
+from textblob import TextBlob
 
-st.title("Hello, Streamlit!")
-st.write("이건 Streamlit으로 만든 간단한 웹 앱입니다.!")
+# 제목
+st.title("댓글 감정 분석 웹 앱")
 
-name = st.text_input("이름을 입력하세요:")
-if st.button("인사하기"):
-    st.write(f"안녕하세요, {name}님! Streamlit에 오신 것을 환영합니다. 반가워요~")
+#댓글 입력받기
+comment = st.text_area("댓글을 입력하세요:")
+comment = comment.strip()
+
+#댓글 분석 버튼
+if st.button("분석하기"):
+    if comment:
+        # 감정 분석 수행
+        analysis = TextBlob(comment)
+        sentiment_score = analysis.sentiment.polarity
+        st.info(f"점수 = {sentiment_score}")
+        
+        # 감정 결과 표시
+        if sentiment_score > 0.1:
+            st.success("긍정적인 댓글입니다! 😊")
+        elif sentiment_score < -0.1:
+            st.error("부정적인 댓글입니다. 😢")
+        else:
+            st.info("중립적인 댓글입니다. 😐")
+    else:
+        st.warning("댓글을 입력해주세요.")
